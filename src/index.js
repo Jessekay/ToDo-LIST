470a788eca94b4002fa069b82fb07861f549087c
Jesse <kayjes00@gmail.com>
2023-06-14 18:51:18 +0100

    modify index.html

--- src/index.js
+++ src/index.js
@@ -1,45 +1,47 @@
 import './index.css';
-import {
-  getTodoList, addItem, removeItem, removeCompletedItems, editItem,
-} from './todolist.js';
-import displayToDoList from './dis.js';
 
-const inputItem = document.querySelector('.field-input');
-const addButton = document.querySelector('.add-btn');
-const clearAll = document.querySelector('#clear-btn');
+// Get DOM
+const listElement = document.createElement('div');
+listElement.classList = 'dynamic-container';
+const listContainer = document.querySelector('.list');
 
-let editIndex = -1;
-addButton.addEventListener('click', () => {
-  if (editIndex === -1) {
-    addItem(inputItem.value);
-  } else {
-    editItem(editIndex.toExponential, inputItem.value);
-    editIndex = -1;
-    addButton.textContent = 'Add';
-  }
-  inputItem.value = '';
-  displayToDoList(getTodoList());
-});
-clearAll.addEventListener('click', () => {
-  removeCompletedItems();
-  displayToDoList(getTodoList());
-});
+const task = [
+  {
+    description: 'task1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'task4',
+    completed: false,
+    index: 4,
+  },
+];
+function iterateTasks() {
+  task.forEach((item) => {
+    listElement.innerHTML
+        += `
+        <div class="list-properties">
+            <span class="left-items">
+                <input type="checkbox">
+                <p>${item.description}</p>
+            </span>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+        <hr>
+        `;
+  });
 
-// Event listener for removing an item
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove-btn')) {
-    const itemIndex = parseInt(event.target.dataset.index, 10);
-    removeItem(itemIndex);
-    displayToDoList(getTodoList());
-  }
-});
-// Event listener for editing an item
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('edit-btn')) {
-    const itemIndex = parseInt(event.target.dataset.index, 10);
-    inputItem.value = getTodoList()[itemIndex].description;
-    editIndex = itemIndex;
-    addButton.textContent = 'Save';
-  }
-});
-displayToDoList(getTodoList());
+  listContainer.appendChild(listElement);
+}
+iterateTasks();
