6f662526d767601ecd6fef6681d94ac981c12675
Jesse <kayjes00@gmail.com>
2023-06-15 11:03:26 +0100

    implement a function to add item

--- src/index.js
+++ src/index.js
@@ -1,47 +1,26 @@
 import './index.css';
+import {
+  getTodoList, addItem, removeItem, removeCompletedItems, editItem,
+} from './todolist.js';
+import displayToDoList from './dis.js';
 
-// Get DOM
-const listElement = document.createElement('div');
-listElement.classList = 'dynamic-container';
-const listContainer = document.querySelector('.list');
+const inputItem = document.querySelector('.field-input');
+const addButton = document.querySelector('.add-btn');
+const clearAll = document.querySelector('#clear-btn');
 
-const task = [
-  {
-    description: 'task1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'task2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'task3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'task4',
-    completed: false,
-    index: 4,
-  },
-];
-function iterateTasks() {
-  task.forEach((item) => {
-    listElement.innerHTML
-        += `
-        <div class="list-properties">
-            <span class="left-items">
-                <input type="checkbox">
-                <p>${item.description}</p>
-            </span>
-            <i class="fa-solid fa-ellipsis-vertical"></i>
-        </div>
-        <hr>
-        `;
-  });
-
-  listContainer.appendChild(listElement);
-}
-iterateTasks();
+let editIndex = -1;
+addButton.addEventListener('click', () => {
+  if (editIndex === -1) {
+    addItem(inputItem.value);
+  } else {
+    editItem(editIndex.toExponential, inputItem.value);
+    editIndex = -1;
+    addButton.textContent = 'Add';
+  }
+  inputItem.value = '';
+  displayToDoList(getTodoList());
+});
+clearAll.addEventListener('click', () => {
+  removeCompletedItems();
+  displayToDoList(getTodoList());
+});
